1faf2fb3e349585a5947a40365104014f140d781
Jacob Duden <Jlavaball@gmail.com>
2021-03-12 20:01:50 -0600

    made the fight function and asked user for input

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,6 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("what is your robot's name?");
+console.log(playerName);
+function fight() {
+    window.alert("The fight has begun!")
+}
+// fight();
